54d5ba82d9e19ded8bcb7b7f5d5dc27c8fc008ce
Halid Cisse <halidoucse@gmail.com>
2021-07-26 15:51:43 +0100

    v

---
+++ Exam2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exam2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var carsPositions1 = new List<long>{2, 10, 8, 17};
+            var numberOfCarsToCover1 = 3;
+
+            var carsPositions2 = new List<long> { 1, 2, 3, 10 };
+            var numberOfCarsToCover2 = 4;
+
+
+            var result = CarParkingRoof(carsPositions1, numberOfCarsToCover1);
+            //var result = CarParkingRoof(carsPositions2, numberOfCarsToCover2);
+            Console.WriteLine($"Done! {result}");
+        }
+
+        public static long CarParkingRoof(List<long> carsPositions, int numberOfCarsToCover)
+        {
+            var min = carsPositions.Min();
+            var max = carsPositions.Max();
+            var shortestCoverLength = (max - min) + 1;
+
+            foreach (var carPosition in carsPositions)
+            {
+                // for each position calculate position to others
+
+                foreach (var neighborPosition in carsPositions.Except(new[] {carPosition}))
+                {
+                    // calculate coverLength
+                    var coverLength = Math.Abs(neighborPosition - carPosition) + 1;
+                    var coveredCars = 0;
+
+                    if (neighborPosition > carPosition)
+                    {
+                        for (var i = carPosition; i <= neighborPosition; i++)
+                        {
+                            if (carsPositions.Any(e=> e == i))
+                            {
+                                coveredCars++;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        for (var i = carPosition; i <= neighborPosition; i--)
+                        {
+                            if (carsPositions.Any(e => e == i))
+                            {
+                                coveredCars++;
+                            }
+                        }
+                    }
+
+                    if (shortestCoverLength > coverLength && coveredCars == numberOfCarsToCover)
+                    {
+                        shortestCoverLength = coverLength;
+                        Console.WriteLine($"Found {carPosition}->{neighborPosition} {shortestCoverLength}");
+                    }
+                }
+            }
+
+            Console.WriteLine($"Found {shortestCoverLength}");
+            return shortestCoverLength;
+        }
+    }
+}
---
+++ Exam3/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exam3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var arrival = new List<int>{ 1, 3, 5 };
+            //var durations = new List<int>{ 2, 2, 2 };
+
+            //var arrival = new List<int> { 1, 3, 3, 5, 7 };
+            //var durations = new List<int> { 2, 2, 1, 2, 1 };
+
+            var arrival = new List<int> { 1};
+            var durations = new List<int> { 1 };
+
+            //var result = MaxEvents(arrival1, durations1);
+            var result = MaxEvents(arrival, durations);
+            Console.WriteLine($"Result {result}");
+        }
+
+        public static int MaxEvents(List<int> arrivals, List<int> durations)
+        {
+            // Code was written in vs code
+            // Code is writing to be correct not to be fast
+            // find conflicting events
+            // Then subtract them
+
+            var conflicts = 0;
+
+            for (var i = 0; i < arrivals.Count - 1; i++)
+            {
+                var arrival = arrivals[i];
+
+                var arrivalTime = arrival + durations[i];
+                var nextTime = arrivals[i + 1] + durations[i + 1];
+            }
+
+            for (var i = 0; i < arrivals.Count - 1; i++)
+            {
+                var arrival = arrivals[i];
+
+                var arrivalTime = arrival + durations[i];
+                var previewTime = arrivals[i - 1] + durations[i - 1];
+                var nextTime = arrivals[i + 1] + durations[i + 1];
+
+                if (arrivalTime < previewTime)
+                {
+                    conflicts++;
+                }
+
+                if (arrivalTime > nextTime)
+                {
+                    conflicts++;
+                }
+            }
+
+            return arrivals.Count - conflicts;
+        }
+    }
+}
